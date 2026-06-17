ab7935a0ac5dd5757b6d524f6a7bc043b4a82fac
HC2004 <100126660+HC2004@users.noreply.github.com>
2026-06-17 10:36:48 +0400

    Update app.js

--- server/app.js
+++ server/app.js
@@ -20,7 +20,13 @@ if (SESSION_SECRET === 'change-me-in-production-please') {
       'Задай свой секрет в файле .env перед тем, как выкладывать сайт в реальный интернет.\n'
   );
 }
-
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*"); // В идеале здесь должен быть твой домен, но для теста сойдет "*"
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.header("Access-Control-Allow-Methods", "POST, OPTIONS");
+  if (req.method === 'OPTIONS') return res.sendStatus(200);
+  next();
+});
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
